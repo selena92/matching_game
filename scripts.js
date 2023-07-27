864c87d453de5635e6f8fefed7073d1cc5b8ca92
selena.chumber <selena.chumber@ten10.com>
2023-07-27 09:30:21 +0100

    updated java functions and html document

--- scripts.js
+++ scripts.js
@@ -1,18 +1,45 @@
 // scripts.js
-const cards = document.querySelectorAll('.memory-card');
+ const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
-let firstCard, secondCard;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
 
-function flipCard() {
-  this.classList.add('flip');
+  function flipCard() {
+    if (lockBoard) return;
+    this.classList.add('flip');
 
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+     return;
+   }
 
-if (!hasFlippedCard) {
-  hasFlippedCard = true;
-  firstCard = this;
+   secondCard = this;
+   hasFlippedCard = false;
+
+   checkForMatch();
  }
-}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+ function checkForMatch() {
+   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+   isMatch ? disableCards() : unflipCards();
+ }
 
+ function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+ }
+
+ function unflipCards() {
+     lockBoard = true;    
+
+   setTimeout(() => {
+     firstCard.classList.remove('flip');
+     secondCard.classList.remove('flip');
+  
+     lockBoard = false;
+   }, 1500);
+ }
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
